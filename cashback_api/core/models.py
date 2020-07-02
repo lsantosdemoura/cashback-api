53369d999560561ed42c0f897914931e5bc8ff64
Lucas Moura <lsantosdemoura99@gmail.com>
2020-07-02 03:06:58 -0300

    Models refactoring.

--- cashback_api/core/models.py
+++ cashback_api/core/models.py
@@ -1,37 +1,42 @@
 from django.db import models
 from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.base_user import AbstractBaseUser
-from django.utils.translation import ugettext_lazy as _
 from localflavor.br.models import BRCPFField
 
 from .managers import UserManager
 
 
 class User(AbstractBaseUser, PermissionsMixin):
-    email = models.EmailField(_('email address'), unique=True)
-    fullname = models.CharField(_('full name'), max_length=30, blank=True)
-    date_joined = models.DateTimeField(_('date joined'), auto_now_add=True)
-    cpf = BRCPFField(verbose_name='CPF', unique=True, db_index=True)
-    is_active = models.BooleanField(_('active'), default=True)
-    is_staff = models.BooleanField(_('staff'), default=True)
+    cpf = BRCPFField(verbose_name='CPF', primary_key=True, db_index=True)
+    email = models.EmailField('email address', unique=True)
+    fullname = models.CharField('full name', max_length=30, blank=False)
+    date_joined = models.DateTimeField('date joined', auto_now_add=True)
+    is_active = models.BooleanField('active', default=True)
+    is_staff = models.BooleanField('staff', default=False)
+
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
+    REQUIRED_FIELDS = ['fullname', 'cpf']
 
     def __str__(self):
         return self.email
 
     class Meta:
-        verbose_name = _('reseller')
-        verbose_name_plural = _('resellers')
+        verbose_name = 'user'
+        verbose_name_plural = 'users'
 
 
-class Shopping(models.Model):
+class Purchase(models.Model):
     code = models.CharField(max_length=150, blank=False, null=False)
     value = models.DecimalField(
         max_digits=20, decimal_places=2, blank=False, null=False
     )
     date = models.DateField(blank=False, null=False)
     reseller = models.ForeignKey(to=User, on_delete=models.CASCADE)
-    status = models.CharField(max_length=12, blank=False, null=False)
+    status = models.CharField(
+        max_length=12, blank=False, null=False, default='Em validação'
+    )
+
+    def __str__(self):
+        return self.code
